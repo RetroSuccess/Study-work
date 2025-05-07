1edec9094ebc747b0cf8e3dc7fbf9e239581f6ad
RetroSuccess <165993473+RetroSuccess@users.noreply.github.com>
2025-05-07 09:02:42 +0200

    Add files via upload

---
+++ New/Prac6.js
@@ -0,0 +1,7 @@
+window.alert("Welcome to webtech");
+var name = prompt("What is your name?");
+let surname = prompt("Enter your surname");
+window.alert(name);
+console.log(surname);
+document.write(`Welcome ${name} ${surname}`)
+
